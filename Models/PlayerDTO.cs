6efbfa2cc18ae1bd66cdebe2f8c124a240848d4a
Wilson Carvajal <wilson@corporaciondpt.com>
2020-04-28 15:23:59 -0600

    inicial commit

---
+++ Models/PlayerDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBronto.Models
+{
+    public class PlayerDTO
+    {
+        public int IdPlayer { set; get; }
+        public string Player { set; get; }
+        public string Email { set; get; }
+        public DateTime AccountOpened { set; get; }
+        public decimal CurrentBalance { set; get; }
+        public decimal Days { set; get; }
+
+        public override string ToString()
+        {
+            return $"IdPlayer: {IdPlayer} ,Player: {Player} ,Email: {Email} ,AccountOpened: {AccountOpened},CurrentBalance: {CurrentBalance} , Days: {Days}   ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using ConsoleBronto.Models;
+using WagerCoreWebAPI.Utils.Http;
+
+namespace ConsoleBronto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetPlayers();
+        }
+        public static void GetPlayers()
+        {
+            try
+            {
+                List<PlayerDTO> PlayerList = new List<PlayerDTO>();
+                PlayerDTO ObjPlayer = new PlayerDTO();
+                int contador = 0;
+
+                PlayerList =  new GenericHttpJson().PostEventResponse<List<PlayerDTO>, PlayerDTO> (ObjPlayer, "https://localhost:44346/api/Bronto/GetPlayers ", " ");
+
+                foreach (var item in PlayerList)
+                {
+                    Console.WriteLine(item.ToString());
+                    contador++;
+                }
+                Console.WriteLine("Total : " + contador);
+                Thread.Sleep(50000000);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
